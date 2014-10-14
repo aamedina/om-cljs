29042e298f7796b094d07ef056662d8bce99726e
Eliana Medina <emwalker1234@gmail.com>
2014-10-14 16:15:21 -0400

    added dommy and updated all usages

--- src/leiningen/new/om_cljs/src/core.cljs
+++ src/leiningen/new/om_cljs/src/core.cljs
@@ -2,10 +2,9 @@
     (:require [weasel.repl :as repl]
               [cljs.core.async :as a :refer [<! >! put! take! chan]]
               [om.core :as om :include-macros true]
-              [sablono.core :as html :refer [html] :include-macros true]
-              [dommy.core :as dom])
-    (:require-macros [cljs.core.async.macros :as a :refer [go go-loop]]
-                     [dommy.macros :refer [sel sel1 node deftemplate]]))
+              [sablono.core :as html :refer-macros [html] :include-macros true]
+              [dommy.core :as dom :refer-macros [sel sel1]])
+    (:require-macros [cljs.core.async.macros :as a :refer [go go-loop]]))
 
 (enable-console-print!)
 
